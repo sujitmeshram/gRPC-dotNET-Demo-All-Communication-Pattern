c2820e80ded70203af41a53095c0bc0d1d332658
sujitmeshram <sujitmeshramofficial@gmail.com>
2024-06-29 20:22:57 +0530

    add all files

---
+++ gRPC dotnet demo/client/gRPC.Client/gRPC.Client/Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Google.Protobuf.WellKnownTypes;
+using gRPC.net.demo.stockDetails;
+using System.Threading;
+using Grpc.Core;
+using Grpc.Health.V1;
+
+namespace gRPC.Client
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new StockService.StockServiceClient(channel);
+            Metadata headers = await FetchAuthenticationToken(client);
+
+            while (true)
+            {
+                try
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkGreen;
+                    Console.WriteLine("******************************************");
+                    Console.WriteLine("Press 1 to GetStockListings");
+                    Console.WriteLine("Press 2 to GetStockPrice");
+                    Console.WriteLine("Press 3 to GetStockPriceStream");
+                    Console.WriteLine("Press 4 to GetStocksPrices");
+                    Console.WriteLine("Press 5 to GetCompanyStockPriceStream");
+                    Console.WriteLine("Press 6 to check service health");
+                    Console.WriteLine("Press 7 to Exit from app");
+                    Console.WriteLine("******************************************");
+                    Console.ResetColor();
+                    var input = Console.ReadLine();
+                    switch (input)
+                    {
+                        case "1":
+                            await GetStockListings(client, headers);
+                            break;
+                        case "2":
+                            await GetStockPrice(client, headers);
+                            break;
+                        case "3":
+                            await GetStockPriceStream(client, headers);
+                            break;
+                        case "4":
+                            await GetStocksPrices(client, headers);
+                            break;
+                        case "5":
+                            await GetCompanyStockPriceStream(client, headers);
+                            break;
+                        case "6":
+                            await CheckServiceHealth(channel);
+                            break;
+                        case "7":
+                            Environment.Exit(0);
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                catch (RpcException ex) when (ex.StatusCode == StatusCode.Unauthenticated)
+                {
+                    //Assuming token expired, renewing..
+                    Console.WriteLine("Token expired/rejected. Fetching latest...");
+                    headers = await FetchAuthenticationToken(client);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error in app: " + ex);
+                }
+            }
+        }
+
+        private static async Task GetStockListings(StockService.StockServiceClient client, Metadata headers)
+        {
+            var result = await client.GetStockListingsAsync(new Empty(), headers: headers);
+            foreach (var stock in result.Stocks)
+            {
+                Console.WriteLine($"{stock.StockId}\t{stock.StockName}");
+            }
+        }
+
+        private static async Task GetStockPrice(StockService.StockServiceClient client, Metadata headers)
+        {
+            var result = await client.GetStockPriceAsync(new Stock { StockId = "FB" }, headers: headers);
+            Console.WriteLine($"Stock Id: {result.Stock.StockId}\n Stock Name: {result.Stock.StockName}\n " +
+                $"Stock Price: {result.Price}\n TimeStamp: {result.DateTimeStamp}");
+        }
+
+        private static async Task GetStockPriceStream(StockService.StockServiceClient client, Metadata headers)
+        {
+            var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
+
+            using var streamingCall = client.GetStockPriceStream(new Empty(), cancellationToken: cts.Token, headers: headers);
+            try
+            {
+                Console.WriteLine(string.Format($"Stock Id\t Stock Name\t Stock Price\t TimeStamp"));
+                await foreach (var stockPrice in streamingCall.ResponseStream.ReadAllAsync(cancellationToken: cts.Token))
+                {
+                    Console.WriteLine(string.Format($"{stockPrice.Stock.StockId}\t {stockPrice.Stock.StockName}\t {stockPrice.Price}\t {stockPrice.DateTimeStamp}"));
+                }
+            }
+            catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+            {
+                Console.WriteLine("Stream cancelled.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error reading response: " + ex);
+            }
+        }
+
+        private static async Task GetStocksPrices(StockService.StockServiceClient client, Metadata headers)
+        {
+            using var streamingCall = client.GetStocksPrices(headers: headers);
+
+            //send requests through requst stream
+            foreach (var stockId in new[] { "FB", "AAPL", "AMZN", "MSFT", "GOOG" })
+            {
+                Console.WriteLine($"Requesting details for {stockId}...");
+
+                await streamingCall.RequestStream.WriteAsync(new Stock { StockId = stockId });
+
+                //Mimicing delay in sending request
+                await Task.Delay(1500);
+            }
+
+            Console.WriteLine("Completing request stream");
+            await streamingCall.RequestStream.CompleteAsync();
+            Console.WriteLine("Request stream completed");
+
+            var response = await streamingCall;
+            foreach (var stockPrice in response.StockPriceList)
+            {
+                Console.WriteLine(string.Format($"{stockPrice.Stock.StockId}\t {stockPrice.Stock.StockName}\t {stockPrice.Price}\t {stockPrice.DateTimeStamp}"));
+            }
+            Console.WriteLine();
+        }
+                
+        private static async Task GetCompanyStockPriceStream(StockService.StockServiceClient client, Metadata headers)
+        {
+            //To get all stock Ids
+            //var stocks = client.GetStockListings(new Empty()).Stocks; 
+            using var streamingCall = client.GetCompanyStockPriceStream(headers: headers);
+
+            // background task which uses async streams to read each stockPrice from the response steam.
+            _ = Task.Run(async () =>
+            {
+                try
+                {
+                    await foreach (var stockPrice in streamingCall.ResponseStream.ReadAllAsync())
+                    {
+                        Console.WriteLine(string.Format($"{stockPrice.Stock.StockId}\t {stockPrice.Stock.StockName}\t {stockPrice.Price}\t {stockPrice.DateTimeStamp}"));
+                    }
+                }
+                catch (RpcException ex) when (ex.StatusCode == StatusCode.Cancelled)
+                {
+                    Console.WriteLine("Stream cancelled.");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error reading response: " + ex);
+                }
+            });
+
+            //send requests through requst stream
+            foreach (var stockId in new[] { "FB", "AAPL", "AMZN", "MSFT", "GOOG" })
+            {
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Requesting details for {stockId}...");
+                Console.ResetColor();
+
+                await streamingCall.RequestStream.WriteAsync(new Stock { StockId = stockId });
+
+                //Mimicing delay in sending request
+                await Task.Delay(1500);
+            }
+
+            Console.WriteLine("Completing request stream");
+            await streamingCall.RequestStream.CompleteAsync();
+            Console.WriteLine("Request stream completed");
+        }
+
+        private static async Task CheckServiceHealth(GrpcChannel channel)
+        {
+            var healthClient = new Health.HealthClient(channel);
+
+            var health = await healthClient.CheckAsync(new HealthCheckRequest { Service = "StockDataService" });
+
+            Console.WriteLine($"Health Status: {health.Status}");
+        }
+
+        private static async Task<Metadata> FetchAuthenticationToken(StockService.StockServiceClient client)
+        {
+            var authToken = await client.AuthenticateAsync(new ClientCred { ClientId = "clientId1", ClientSecret = "secret1" });
+            var headers = new Metadata();
+            headers.Add("Authorization", string.Format($"Bearer {authToken.BearerToken}"));
+            return headers;
+        }
+    }
+}
---
+++ gRPC dotnet demo/server/gRPC.Server/gRPC.Server/Startup.cs
@@ -0,0 +1,89 @@
+﻿using gRPC.Server.Authentication;
+using gRPC.Server.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.HealthCheck;
+using gRPC.Server.BackgroundServices;
+
+namespace gRPC.Server
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddGrpc();
+
+            var tokenKey = "This is gRPC demo sample key";
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(x =>
+            {
+                x.RequireHttpsMetadata = false;
+                x.SaveToken = true;
+                x.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(tokenKey)),
+                    ValidateIssuer = false,
+                    ValidateAudience = false
+                };
+            });
+
+            services.AddAuthorization();
+
+            services.AddSingleton<IJWTAuthenticationManager>(new JWTAuthenticationManager(tokenKey));
+
+            // Simple healthcheck by calling "/health" endpoint
+            services.AddHealthChecks();
+            //using "HealthServiceImpl : Health.V1.Health.HealthBase"
+            services.AddSingleton<HealthServiceImpl>();
+            services.AddHostedService<ServingStatusUpdateService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            //The order in which you register the ASP.NET Core authentication middleware matters.
+            //Always call UseAuthentication and UseAuthorization after UseRouting and before UseEndpoints.
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                // Simple healthcheck by calling "/health" endpoint
+                endpoints.MapHealthChecks("/health");
+                //mapping "HealthServiceImpl : Health.V1.Health.HealthBase"
+                endpoints.MapGrpcService<HealthServiceImpl>();
+
+                endpoints.MapGrpcService<StockDataService>();
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+                });
+            });
+        }
+    }
+}
